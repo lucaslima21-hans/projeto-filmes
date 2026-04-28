528ecc99790cdf7cdda251acdc418f9d3f150938
sarah! <sarah.c.nascimento10@gmail.com>
2026-04-28 15:44:43 -0300

    28-04
    adicionei um readme, apenas para ficar 'completo' , em breve pretendo mudar e arrumar, estou tentando fazer o arquivo do services, soq não tá legal, não tá dando certo esse treco = desisti, dps eu vejo se continuo

---
+++ src/services/filmesServices.js
@@ -0,0 +1,8 @@
+//aqui fica toda a logica de negócio, ou seja, tudo o que tem a ver com o banco de dados
+
+const filmes = [
+    {"id" : 1 , "titulo" : "Bebe chefinho", "categoria" : "animação infantil"},
+    {"id" : 2 , "titulo" : "Se Nao Fosse Voce", "categoria" : "comedia"},
+    {"id" : 3 , "titulo" : "Ze Colmeia", "categoria" : "animação infantil"},
+    {"id" : 4 , "titulo": "Scooby Doo" , "categoria" : "animação infantil"}
+]
