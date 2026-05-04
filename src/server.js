3f826cda8bd8ad1ba39cbb52282789976aa6fb7c
lucaslima21-hans <lucaslima21032008@gmail.com>
2026-05-04 15:51:12 -0300

    server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json()); 
-
-app.listen(3000, () => {
-    console.log('servidor rodando na porta 3000');
-})
+import express from "express";
